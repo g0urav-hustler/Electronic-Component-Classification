076ee8d9ce3ac90e99d24d7679122884ded6400a
g0urav-hustler <gouravchouhan966@gmail.com>
2023-04-11 15:56:10 +0530

    read_params.py

---
+++ src/read_params.py
@@ -0,0 +1,16 @@
+# read params
+# process 
+# return dataframe
+
+import yaml, argparse
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default= "params.yaml")
+    parsed_args = args.parse_args()
